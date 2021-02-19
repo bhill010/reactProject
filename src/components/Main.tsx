b59da89ee87ec86d66ac97d6a1fd261877c9664c
Brandon Hill <bmhill@fb.com>
2021-02-19 01:48:41 -0800

    Add status headers and subheaders

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,8 +10,36 @@ interface MainProps {
 export const Main: React.FC<MainProps> = ({ selectedService }) => {
   return (
     <div>
-      <div>===============</div>
+      <div>==========================</div>
+      <div>==========================</div>
+      <div>{serviceStatusHeader(selectedService)}</div>
+      <div>{serviceStatusSubHeader(selectedService)}</div>
       <div>{selectedService.name}</div>
     </div>
   );
+};
+
+const serviceStatusHeader = (selectedService: Service): string => {
+  let status = "No service disruptions";
+
+  if (!selectedService.lineStatuses.length) {
+    return status;
+  }
+
+  for (let serviceType of selectedService.lineStatuses) {
+    if (serviceType.statusSeverity !== 10) {
+      status = "Service currently suffering disruptions";
+      return status;
+    }
+  }
+
+  return status;
+};
+
+const serviceStatusSubHeader = (selectedService: Service): JSX.Element[] => {
+  return selectedService.lineStatuses
+    .filter((selectedService) => selectedService.statusSeverity !== 10)
+    .map((selectedService) => {
+      return <li>{selectedService.reason}</li>;
+    });
 };
